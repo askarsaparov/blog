a949a0c715d6d392fa0891da43263e838a11e4fa
askarsaparov <asqarsaparov2303@gmail.com>
2023-05-08 00:15:59 +0500

    bugfix-deploy config

--- my_blog/settings.py
+++ my_blog/settings.py
@@ -131,6 +131,7 @@ USE_I18N = True
 USE_TZ = True
 
 CSRF_TRUSTED_ORIGINS = ['https://myblog-production-21b3.up.railway.app/']
+CSRF_COOKIE_SECURE = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
